834eff03c6182f19ac4e87aa44cdf13512968210
hridoy8299 <hridoy8299@gmail.com>
2022-08-09 21:01:16 +0600

    second  commit

---
+++ 6_array_index.js
@@ -0,0 +1,9 @@
+/*You are given an array:
+var fruits = ['Apple', 'Banana', 'Orange'];
+a) Find the index of ‘Banana’ and replace ‘Banana’ with ‘Mango’.
+b) Remove ‘Orange’ and add ‘Watermelon’. */
+
+var fruits =['Apple', 'Banana', 'Orange'];
+
+var position =fruits.indexOf('Banana');
+console.log(position);
